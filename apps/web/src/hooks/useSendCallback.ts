3fa8e1ab9190d113e93b65037acaa2a7639bd236
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-07-31 12:54:23 +0200

    feat: send preconfirmation using eth_sign

--- apps/web/src/hooks/useSendCallback.ts
+++ apps/web/src/hooks/useSendCallback.ts
@@ -1,26 +1,20 @@
+import * as EthereumJS from '@ethereumjs/tx'
 import { TransactionRequest } from '@ethersproject/abstract-provider'
-import { BigNumber } from '@ethersproject/bignumber'
-import { left } from '@popperjs/core'
 import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 import { useSupportedChainId } from 'constants/chains'
-import { utils } from 'ethers'
-import { hexlify, keccak256, parseUnits, recoverAddress, recoverPublicKey, RLP, splitSignature } from 'ethers/lib/utils'
+import { keccak256, parseUnits, splitSignature } from 'ethers/lib/utils'
 import { useAccount } from 'hooks/useAccount'
 import { useEthersProvider } from 'hooks/useEthersProvider'
 import { useSwitchChain } from 'hooks/useSwitchChain'
 import { GasFeeResult } from 'hooks/useTransactionGasFee'
-import { toEvenLengthHex, toHexPrefixed } from 'lib/utils/hex'
-import { useCallback, useState } from 'react'
+import { toHexPrefixed } from 'lib/utils/hex'
+import { useCallback } from 'react'
 import { useTransactionAdder } from 'state/transactions/hooks'
 import { trace } from 'tracing/trace'
 import { UniverseChainId } from 'uniswap/src/types/chains'
 import { UserRejectedRequestError, toReadableError } from 'utils/errors'
 import { didUserReject } from 'utils/swapErrorToUserReadableMessage'
-import * as EthereumJS from "@ethereumjs/tx"
-import { recoverTransactionAddress } from 'viem'
-import { signTransaction } from 'viem/_types/accounts/utils/signTransaction'
-import { getWalletClient } from 'wagmi/actions'
-import { useWalletClient } from 'wagmi'
+import { hexToBytes } from 'viem'
 
 export function useSendCallback({
   currencyAmount,
@@ -38,8 +32,6 @@ export function useSendCallback({
   const addTransaction = useTransactionAdder()
   const switchChain = useSwitchChain()
   const supportedTransactionChainId = useSupportedChainId(transactionRequest?.chainId)
-  const [nonceWithPreconfs, setNonceWithPreconfs] = useState(0);
-  const walletClient = useWalletClient();
 
   return useCallback(
     () =>
@@ -64,20 +56,16 @@ export function useSendCallback({
         // }
 
         const nonce = await provider!.getTransactionCount(account.address!)
-        if (nonceWithPreconfs === 0) setNonceWithPreconfs(nonce)
-
-        console.log("nonceWithPreconfs", nonceWithPreconfs)
-
 
         let txReq = EthereumJS.FeeMarketEIP1559Transaction.fromTxData({
           chainId: UniverseChainId.Helder,
-          nonce: nonce,
+          nonce,
           gasLimit: 42000,
           maxFeePerGas: parseUnits('2', 'gwei').toBigInt(),
           maxPriorityFeePerGas: parseUnits('1', 'gwei').toBigInt(),
-          to: '0xaF05F88369c4fEbF96f2B2fa046E831De174Ee95',
+          to: '0xaf05f88369c4febf96f2b2fa046e831de174ee95',
           value: parseUnits('0.001', 'ether').toBigInt(),
-          data: '0xB017B017B017B017B017B017B017B017B017B017B017B017B017',
+          data: '0xb017b017b017b017b017b017b017b017b017b017b017b017b017',
         })
 
         try {
@@ -85,50 +73,38 @@ export function useSendCallback({
             { name: 'Send transaction', op: 'wallet.send_transaction' },
             async (walletTrace) => {
               try {
-                // if (account.chainId !== supportedTransactionChainId) {j
+                // if (account.chainId !== supportedTransactionChainId) {
                 //   await switchChain(supportedTransactionChainId!)
                 // }
 
-                const preparedTxRequest = await walletClient.data!.prepareTransactionRequest({
-                  account: account.address,
-                  chainId: account.chainId,
-                  maxFeePerGas: parseUnits('2', 'gwei').toBigInt(),
-                  to: '0xaF05F88369c4fEbF96f2B2fa046E831De174Ee95',
-                  maxPriorityFeePerGas: parseUnits('1', 'gwei').toBigInt(),
-                  value: parseUnits('0.001', 'ether').toBigInt(),
-                  data: '0xB017B017B017B017B017B017B017B017B017B017B017B017B017',
-                  nonce: nonce,
+                const signer = provider!.getSigner()
+
+                // https://eips.ethereum.org/EIPS/eip-1559
+                // The signature_y_parity, signature_r, signature_s elements of this transaction represent a secp256k1 signature over
+                // keccak256(0x02 || rlp([chain_id, nonce, max_priority_fee_per_gas, max_fee_per_gas, gas_limit, destination, amount, data, access_list])).
+                const serializedTx = txReq.getMessageToSign()
+                const keccakSerializedTx = hexToBytes(keccak256(serializedTx) as any)
+                const signature = await signer._legacySignMessage(keccakSerializedTx)
+                console.log('signature', signature)
+
+                const sigSplitted = splitSignature(signature)
+
+                txReq = EthereumJS.FeeMarketEIP1559Transaction.fromTxData({
+                  ...txReq,
+                  v: 0,
+                  r: sigSplitted.r,
+                  s: sigSplitted.s,
                 })
-                console.log("before signTransaction", preparedTxRequest)
-                const rawTransaction = await walletClient.data!.signTransaction(preparedTxRequest)
-                console.log("rawTransaction", rawTransaction)
 
-                // const signer = provider!.getSigner()
-                // // 0x02f88f8501a2140cff038503b9aca0008503b9aca00082520894af05f88369c4febf96f2b2fa046e831de174ee9584010000009ab017b017b017b017b017b017b017b017b017b017b017b017b017c001a0d2f19f42e52656f1886c42bdc588297cbb812041c3bbcf76cf4e2a833fb80cfaa048dc78ad6177223a85eab12e735a3e5afcfdb56add259430c764c6003364408
+                const rawTransaction = toHexPrefixed(txReq.serialize())
+                const recoveredSender = toHexPrefixed(txReq.getSenderAddress().toBytes())
+                if (recoveredSender.toLowerCase() !== account.address!.toLowerCase()) {
+                  console.error('Recovered sender does not match account address', recoveredSender, account.address)
+                  return
+                }
 
-                // // https://eips.ethereum.org/EIPS/eip-1559
-                // // The signature_y_parity, signature_r, signature_s elements of this transaction represent a secp256k1 signature over 
-                // // keccak256(0x02 || rlp([chain_id, nonce, max_priority_fee_per_gas, max_fee_per_gas, gas_limit, destination, amount, data, access_list])).
-                // const serializedTx = txReq.getMessageToSign();
-                // console.log("serializedTx", toHexPrefixed(serializedTx), "txreq", txReq)
-                // const keccakSerializedTx = keccak256(serializedTx);
-                // console.log("keccakSerializedTx", keccakSerializedTx)
-
-                // const sig = await signer.signMessage(keccakSerializedTx);
-                // const sigSplitted = splitSignature(sig);
-
-                // console.log('sigSplitted', sigSplitted);
-
-                // txReq = EthereumJS.FeeMarketEIP1559Transaction.fromTxData({ ...txReq, v: sigSplitted.recoveryParam, r: sigSplitted.r, s: sigSplitted.s });
-
-                // const rawTransaction = toHexPrefixed(txReq.serialize())
-                // console.log('rawTransaction', rawTransaction);
-                // console.log("sender", toHexPrefixed(txReq.getSenderAddress().toBytes()))
-
-
-                const hash: string = await provider?.send('eth_sendRawTransaction', [rawTransaction]);
-                setNonceWithPreconfs((prev) => prev + 1)
-                console.log("hash", hash)
+                const hash: string = await provider?.send('eth_sendRawTransaction', [rawTransaction])
+                console.log('tx hash', hash)
                 return hash
               } catch (error) {
                 if (didUserReject(error)) {
